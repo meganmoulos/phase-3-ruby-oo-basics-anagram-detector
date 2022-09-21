de99f5a85ed6a0d429287292be898490e1ab53cf
Megan <meganmoulos@gmail.com>
2022-09-21 10:12:20 -0700

    done with lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+    attr_reader :target
+
+    def initialize(word)
+        @target = word
+    end
+
+    def match(str_arr)
+        str_arr.filter {|word| 
+            word.chars.sort == self.target.chars.sort
+        }
+    end
+
+end
